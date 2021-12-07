ad92b3d8440547463ec8b0752594c81024fd5a80
Lucas Limousin <84068345+LucasLimou@users.noreply.github.com>
2021-12-07 16:31:36 +0100

    Ajout de la partie Fenêtre Princ + Règle + Jeu

---
+++ src/tchoutchou/jeu.java
@@ -0,0 +1,70 @@
+package tchoutchou;
+
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class jeu extends JDialog {
+
+	private MonPanelJeu contentPanel ;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			jeu dialog = new jeu();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public jeu() {
+		setTitle("Partie de TchouTchou");
+		
+		java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+		setBounds((screenSize.width-900)/2, (screenSize.height-800)/2, 900, 800);
+		
+		contentPanel = new MonPanelJeu(this);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		this.setContentPane(contentPanel);
+		contentPanel.setLayout(null);
+	}
+	
+	public void dessiner(Graphics g) {
+		afficherFond((Graphics2D)g);
+		
+	}
+	
+	public void afficherFond(Graphics2D g) {
+		
+		g.setColor(new Color(0,0,0));
+		g.setStroke(new BasicStroke(10));
+		g.drawRect(150, 150, 200, 200);
+		g.setColor(new Color(0,153,0));
+		g.fillRect(150, 150, 200, 200);
+		g.setColor(new Color(204,0,0));
+		g.setStroke(new BasicStroke(50));
+        g.fillArc(150-120, 150-120, 240, 240, -90, 90);
+        g.setColor(new Color(0,153,0));
+        g.fillArc(150-80, 150-80, 160, 160, -90, 90);
+	}
+	
+
+}
---
+++ src/tchoutchou/regle.java
@@ -0,0 +1,113 @@
+package tchoutchou;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class regle extends JDialog {
+
+	private MonPanelRegle contentPanel ;
+	private int numPage = 1 ;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			regle dialog = new regle();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public regle() {
+		
+		setTitle("Règles");
+		
+		java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+		setBounds((screenSize.width-794)/2, (screenSize.height-550)/2, 794, 550);
+		
+		contentPanel = new MonPanelRegle(this);
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		this.setContentPane(contentPanel);
+		contentPanel.setLayout(null);
+		
+		// On regarde le click
+		contentPanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				formMouseClicked(e);
+			}
+		});
+	
+		
+	}
+	
+	private void formMouseClicked(MouseEvent evt) {
+		 // On récupere les coordonnées du pointeur de la souris dans la fenêtre
+		 int sourisX = evt.getPoint().x;
+		 int sourisY = evt.getPoint().y;
+		 // On fait le changement de repère pour se ramener au ContentPane
+		 
+		 if ((sourisX >= 120 && sourisX <= 160) && (sourisY >= 450 && sourisY <= 490)) {
+			 decrPage();
+		 }
+		 if ((sourisX >= 655 && sourisX <= 695) && (sourisY >= 450 && sourisY <= 490)) {
+			 incrPage();
+		 }
+		 
+		 repaint();
+		 
+	}
+	
+	private void incrPage() {
+		if (numPage <= 7) {
+			numPage++ ; 
+		}
+	}
+	
+	private void decrPage() {
+		if (numPage >= 2) {
+			numPage-- ;
+		}
+	}
+	
+	public void dessiner(Graphics g) {
+		//afficher(g);
+		switch (numPage) {
+		case 1 : g.drawImage(new ImageIcon("ImageTchouTchou/Page1.png").getImage(),0,0, 800,544, null);
+			break;
+		case 2 : g.drawImage(new ImageIcon("ImageTchouTchou/Page2.png").getImage(),0,0, 800,544, null);
+			break;
+		case 3 : g.drawImage(new ImageIcon("ImageTchouTchou/Page3.png").getImage(),0,0, 800,544, null);
+			break;
+		case 4 : g.drawImage(new ImageIcon("ImageTchouTchou/Page4.png").getImage(),0,0, 800,544, null);
+			break;
+		case 5 : g.drawImage(new ImageIcon("ImageTchouTchou/Page5.png").getImage(),0,0, 800,544, null);
+			break;
+		case 6 : g.drawImage(new ImageIcon("ImageTchouTchou/Page6.png").getImage(),0,0, 800,544, null);
+			break;
+		case 7 : g.drawImage(new ImageIcon("ImageTchouTchou/Page7.png").getImage(),0,0, 800,544, null);
+			break;
+		case 8 : g.drawImage(new ImageIcon("ImageTchouTchou/Page8.png").getImage(),0,0, 800,544, null);
+			break;
+		}
+	}
+	
+
+}
